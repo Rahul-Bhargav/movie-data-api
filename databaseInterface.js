2c7eaaf67d315b5c367874c70f949be9e6c7025e
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-16 12:54:16 +0530

    improved DB functionalities with tests

--- databaseInterface.js
+++ databaseInterface.js
@@ -10,6 +10,46 @@ const databaseOprations = {
     if (typeof releaseDate !== 'string') return Promise.reject('Release Date is not valid')
     return sequelize.query(`INSERT INTO MOVIE (NAME, RELEASEDATE) VALUES (:name,:releaseDate)`, { replacements: { name: name, releaseDate: releaseDate }, type: sequelize.QueryTypes.INSERT })
   },
+  insertMovies: function (movies) {
+    let values = ''
+    let isValid = true
+    const errorValues = []
+    // add functionality to check for all errors at once
+    movies.forEach((movie, index) => {
+      if (typeof movie.movieName !== 'string') {
+        isValid = false
+        errorValues.push(index)
+      }
+      else if (typeof movie.releaseDate !== 'string') {
+        isValid = false
+        errorValues.push(index)
+      }
+      values += `('${movie.movieName}', '${movie.releaseDate}'), `
+    })
+    if(!isValid)
+      return Promise.reject(`Entries are not valid at indices :${errorValues}`)
+    values = values.slice(0,-2)    
+    // add functionality to have replacements
+    return sequelize.query(`INSERT INTO MOVIE (NAME, RELEASEDATE) VALUES ${values}`, {type: sequelize.QueryTypes.INSERT })
+  },
+  insertActors: function (actors) {
+    let values = ''
+    let isValid = true
+    const errorValues = []
+    // add functionality to check for all errors at once
+    actors.forEach((actor, index) => {
+      if (typeof actor.actorName !== 'string') {
+        isValid = false
+        errorValues.push(index)
+      }
+      values += `('${actor.actorName}'), `
+    })
+    if(!isValid)
+      return Promise.reject(`Entries are not valid at indices :${errorValues}`)
+    values = values.slice(0,-2)    
+    // add functionality to have replacements
+    return sequelize.query(`INSERT INTO ACTORS (NAME) VALUES ${values}`, {type: sequelize.QueryTypes.INSERT })
+  },
   insertActor: function (name) {
     if (typeof name !== 'string') return Promise.reject('Name is not valid')
     return sequelize.query(`INSERT INTO ACTORS (NAME) VALUES (:name)`, { replacements: { name: name }, type: sequelize.QueryTypes.INSERT })
@@ -18,18 +58,30 @@ const databaseOprations = {
     if (typeof movieName !== 'string') return Promise.reject('Movie is not valid')
     if (typeof actor !== 'string') return Promise.reject('Actor Name is not valid')
     return sequelize.query(`INSERT INTO ACTORMOVIE (MOVIE, ACTOR) VALUES (:movieName,:actor)`, { replacements: { movieName: movieName, actor: actor }, type: sequelize.QueryTypes.INSERT })
+  },
+  insertMovieActors: function (movieActors) {
+    console.log('here')
+    let values = ''
+    let isValid = true
+    const errorValues = []
+    // add functionality to check for all errors at once
+    movieActors.forEach((movieActor, index) => {
+      if (typeof movieActor.actorName !== 'string') {
+        isValid = false
+        errorValues.push(index)
+      }
+      if (typeof movieActor.movieName !== 'string') {
+        isValid = false
+        errorValues.push(index)
+      }
+      values += `('${movieActor.movieName}','${movieActor.actorName}'), `
+    })
+    if(!isValid)
+      return Promise.reject(`Entries are not valid at indices :${errorValues}`)
+    values = values.slice(0,-2)    
+    // add functionality to have replacements
+    return sequelize.query(`INSERT INTO ACTORMOVIE (MOVIE, ACTOR) VALUES ${values}`, {type: sequelize.QueryTypes.INSERT })
   }
 }
-
-const checkValidity = function (id, description, status) {
-    if (isFloat(id)) return `id-${id} is not a valid number`
-    if (isNaN(parseInt(id))) return `id-${id} is not a valid number`
-
-    if (typeof description !== 'string') return `description-${description} is not a string`
-
-    if (typeof status !== 'boolean') return `status given is not a boolean`
-
-    return true
-  }
 
 module.exports = databaseOprations
--- test/test-database-Interface.js
+++ test/test-database-Interface.js
@@ -2,7 +2,139 @@ const chai = require('chai')
 const databaseOprations = require('../databaseInterface')
 const expect = chai.expect
 
-describe('insert movies when given valid data ', function () {
+const movies = [
+  {
+    "movieName": "Movie 1",
+    "releaseDate": "Oct-01-2015"
+  },
+  {
+    "movieName": "Movie 2",
+    "releaseDate": "Oct-01-2016"
+  },
+  {
+    "movieName": "Movie 3",
+    "releaseDate": "Nov-01-2016"
+  },
+  {
+    "movieName": "Movie 4",
+    "releaseDate": "Jan-01-2015"
+  },
+  {
+    "movieName": "Movie 5",
+    "releaseDate": "Feb-01-2016"
+  },
+  {
+    "movieName": "Movie 6",
+    "releaseDate": "March-01-2016"
+  }
+]
+
+const invalidMovies = [
+  {
+    "movieName": "Movie 1",
+    "releaseDate": "Oct-01-2015"
+  },
+  {
+    "movieName": 2,
+    "releaseDate": "Oct-01-2016"
+  },
+  {
+    "movieName": "Movie 3",
+    "releaseDate": 3
+  },
+  {
+    "movieName": "Movie 4",
+    "releaseDate": "Jan-01-2015"
+  },
+  {
+    "movieName": "Movie 5",
+    "releaseDate": "Feb-01-2016"
+  },
+  {
+    "movieName": "Movie 6",
+    "releaseDate": "March-01-2016"
+  }
+]
+
+const actors = [
+  {
+    "actorName": "Actor 1",
+    "movies": [
+      "Movie 1",
+      "Movie 2",
+      "Movie 5"
+    ]
+  },
+  {
+    "actorName": "Actor 2",
+    "movies": [
+      "Movie 2",
+      "Movie 3"
+    ]
+  },
+  {
+    "actorName": "Actor 3",
+    "movies": [
+      "Movie 1",
+      "Movie 2",
+      "Movie 3",
+      "Movie 5",
+      "Movie 6"
+    ]
+  }
+]
+
+const invalidactors = [
+  {
+    "actorName": "Actor 1",
+    "movies": [
+      "Movie 1",
+      "Movie 2",
+      "Movie 5"
+    ]
+  },
+  {
+    "actorName": 1,
+    "movies": [
+      "Movie 2",
+      "Movie 3"
+    ]
+  },
+  {
+    "actorName": "Actor 3",
+    "movies": [
+      "Movie 1",
+      "Movie 2",
+      "Movie 3",
+      "Movie 5",
+      "Movie 6"
+    ]
+  }
+]
+
+const movieActors = [
+  {
+    "actorName": "Actor 1",
+    "movieName": "movie 1" 
+  },
+  {
+    "actorName": "Actor 2",
+    "movieName": "movie 1" 
+  }
+]
+
+const invalidmovieActors = [
+  {
+    "actorName": "Actor 1",
+    "movieName": "movie 1" 
+  },
+  {
+    "actorName": 1,
+    "movieName": "movie 1" 
+  }
+]
+
+describe('insert movie when given valid data ', function () {
   it('should return rowcount of insertions', function (done) {
     databaseOprations.insertMovie('movie1', '01-01-1995')
     .then((result)=>{
@@ -12,7 +144,7 @@ describe('insert movies when given valid data ', function () {
   })
 })
 
-describe('insert movies when given invalid data ', function () {
+describe('insert movie when given invalid data ', function () {
   it('should return error', function (done) {
     databaseOprations.insertMovie(1, [])
     .catch((result)=>{
@@ -23,9 +155,29 @@ describe('insert movies when given invalid data ', function () {
 })
 
 
+describe('insert movies when given valid data ', function () {
+  it('should return rowcount of insertions', function (done) {
+    databaseOprations.insertMovies(movies)
+    .then((result)=>{
+      expect(result).to.be.eqls([])
+      done()
+    })
+  })
+})
+
+describe('insert movies when given invalid data ', function () {
+  it('should return error', function (done) {
+    databaseOprations.insertMovies(invalidMovies)
+    .catch((result)=>{
+      expect(result).to.be.eqls('Entries are not valid at indices :1,2')
+      done()
+    })
+  })
+})
+
 describe('insert actors when given valid data ', function () {
   it('should return rowcount of insertions', function (done) {
-    databaseOprations.insertActor('rahul')
+    databaseOprations.insertActors(actors)
     .then((result)=>{
       expect(result).to.be.eqls([])
       done()
@@ -35,6 +187,27 @@ describe('insert actors when given valid data ', function () {
 
 describe('insert actors when given invalid data ', function () {
   it('should return error', function (done) {
+    databaseOprations.insertActors(invalidactors)
+    .catch((result)=>{
+      expect(result).to.be.eqls('Entries are not valid at indices :1')
+      done()
+    })
+  })
+})
+
+
+describe('insert actor when given valid data ', function () {
+  it('should return rowcount of insertions', function (done) {
+    databaseOprations.insertActor('rahul')
+    .then((result)=>{
+      expect(result).to.be.eqls([])
+      done()
+    })
+  })
+})
+
+describe('insert actor when given invalid data ', function () {
+  it('should return error', function (done) {
     databaseOprations.insertActor(1)
     .catch((result)=>{
       expect(result).to.be.eqls('Name is not valid')
@@ -43,7 +216,7 @@ describe('insert actors when given invalid data ', function () {
   })
 })
 
-describe('insert actor movies when given valid data ', function () {
+describe('insert actor movie when given valid data ', function () {
   it('should return rowcount of insertions', function (done) {
     databaseOprations.insertMovieActor('movie1', 'rahul')
     .then((result)=>{
@@ -53,11 +226,31 @@ describe('insert actor movies when given valid data ', function () {
   })
 })
 
-describe('insert actor movies when given invalid data ', function () {
+describe('insert actor movie when given invalid data ', function () {
   it('should return error', function (done) {
     databaseOprations.insertMovieActor(1, 2)
     .catch((result)=>{
       expect(result).to.be.eqls('Movie is not valid')
+      done()
+    })
+  })
+})
+
+describe('insert actor movies when given valid data ', function () {
+  it('should return rowcount of insertions', function (done) {
+    databaseOprations.insertMovieActors(movieActors)
+    .then((result)=>{
+      expect(result).to.be.eqls([])
+      done()
+    })
+  })
+})
+
+describe('insert actor movies when given invalid data ', function () {
+  it('should return error', function (done) {
+    databaseOprations.insertMovieActors(invalidmovieActors)
+    .catch((result)=>{
+      expect(result).to.be.eqls('Entries are not valid at indices :1')
       done()
     })
   })
